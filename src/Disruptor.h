e2543fb503c581de29452421fca4546263ab0f59
unknown <390161495@qq.com>
2019-11-23 11:34:50 +0800

    2019.11.23 add int64_t sequence

--- src/Disruptor.h
+++ src/Disruptor.h
@@ -3,19 +3,20 @@
 #include <array>
 
 #define CACHELINE_SIZE_BYTES 64
-#define CACHELINE_PADDING_FOR_INT64_SIZE (CACHELINE_SIZE_BYTES - sizeof(std::atomic_int64_t))
+#define CACHELINE_PADDING_FOR_ATOMIC_INT64_SIZE (CACHELINE_SIZE_BYTES - sizeof(std::atomic_int64_t))
+#define CACHELINE_PADDING_FOR_INT64_SIZE (CACHELINE_SIZE_BYTES - sizeof(int64_t))
 
 namespace Kang {
 
 	//对std::atomic_int64_t进行了封装，内存补齐保证_seq在一个缓存行中
-	class Sequence
+	class AtomicSequence
 	{
 	public:
-		Sequence(int64_t num = 0L) : _seq(num) {};
-		~Sequence() {};
-		Sequence(const Sequence&) = delete;
-		Sequence(const Sequence&&) = delete;
-		void operator=(const Sequence&) = delete;
+		AtomicSequence(int64_t num = 0L) : _seq(num) {};
+		~AtomicSequence() {};
+		AtomicSequence(const AtomicSequence&) = delete;
+		AtomicSequence(const AtomicSequence&&) = delete;
+		void operator=(const AtomicSequence&) = delete;
 
 		void store(const int64_t val)//, std::memory_order _order = std::memory_order_seq_cst)
 		{
@@ -34,11 +35,37 @@ namespace Kang {
 
 	private:
 		//两边都补齐，以保证_seq不会与其它变量共享一个缓存行
-		char _frontPadding[CACHELINE_PADDING_FOR_INT64_SIZE];
+		char _frontPadding[CACHELINE_PADDING_FOR_ATOMIC_INT64_SIZE];
 		std::atomic_int64_t _seq;
-		char _backPadding[CACHELINE_PADDING_FOR_INT64_SIZE];
+		char _backPadding[CACHELINE_PADDING_FOR_ATOMIC_INT64_SIZE];
 	};
 
+	//对int64_t进行了封装，内存补齐保证_seq在一个缓存行中
+	class Sequence
+	{
+	public:
+		Sequence(int64_t num = 0L) : _seq(num) {};
+		~Sequence() {};
+		Sequence(const Sequence&) = delete;
+		Sequence(const Sequence&&) = delete;
+		void operator=(const Sequence&) = delete;
+
+		void store(const int64_t val)
+		{
+			_seq = val;
+		}
+
+		int64_t load()
+		{
+			return _seq;
+		}
+
+	private:
+		//两边都补齐，以保证_seq不会与其它变量共享一个缓存行
+		char _frontPadding[CACHELINE_PADDING_FOR_INT64_SIZE];
+		int64_t _seq;
+		char _backPadding[CACHELINE_PADDING_FOR_INT64_SIZE];
+	};
 
 	//环形buffer默认大小,为了简化取余操作，这里的长度需要是2的n次方
 	constexpr size_t DefaultRingBufferSize = 262144;
@@ -53,7 +80,7 @@ namespace Kang {
 	class Disruptor
 	{
 	public:
-		Disruptor() : _lastRead(-1L) , _lastWrote(-1L), _lastDispatch(-1L), _writableSeq(0L) , _stopWorking(false){};
+		Disruptor() : _lastRead(-1L) , _lastWrote(-1L), _lastDispatch(-1L), _writableSeq(0L) , _stopWorking(0L){};
 		~Disruptor() {};
 
 		Disruptor(const Disruptor&) = delete;
@@ -67,47 +94,47 @@ namespace Kang {
 		void WriteInBuf(ValueType&& val)
 		{
 			const int64_t writableSeq = _writableSeq.fetch_add(1);
-			while (writableSeq - _lastRead > N)
+			while (writableSeq - _lastRead.load() > N)
 			{//等待策略
-				if (_stopWorking)
-					throw std::runtime_error("writting when stopped disruptor");
-				//std::this_thread::yield();
-			}
-			//写操作
-			_ringBuf[writableSeq & (N - 1)] = std::move(val);
-
-			while (writableSeq - 1 != _lastWrote)
-			{//等待策略
-			}
-			_lastWrote = writableSeq;
-		};
-
-		//向buffer中写内容
-		void WriteInBuf(ValueType& val)
-		{
-			const int64_t writableSeq = _writableSeq.fetch_add(1);
-			while (writableSeq - _lastRead > N)
-			{//等待策略
-				if (_stopWorking)
+				if (_stopWorking.load())
 					throw std::runtime_error("writting when stopped disruptor");
 				//std::this_thread::yield();
 			}
 			//写操作
 			_ringBuf[writableSeq & (N - 1)] = val;
 
-			while (writableSeq - 1 != _lastWrote)
+			while (writableSeq - 1 != _lastWrote.load())
 			{//等待策略
 			}
-			_lastWrote = writableSeq;
+			_lastWrote.store(writableSeq);
+		};
+
+		//向buffer中写内容
+		void WriteInBuf(ValueType& val)
+		{
+			const int64_t writableSeq = _writableSeq.fetch_add(1);
+			while (writableSeq - _lastRead.load() > N)
+			{//等待策略
+				if (_stopWorking.load())
+					throw std::runtime_error("writting when stopped disruptor");
+				//std::this_thread::yield();
+			}
+			//写操作
+			_ringBuf[writableSeq & (N - 1)] = val;
+
+			while (writableSeq - 1 != _lastWrote.load())
+			{//等待策略
+			}
+			_lastWrote.store(writableSeq);
 		};
 
 		//获取可读的buffer下标
 		const int64_t GetReadableSeq()
 		{
 			const int64_t readableSeq = _lastDispatch.fetch_add(1) + 1;
-			while (readableSeq > _lastWrote)
+			while (readableSeq > _lastWrote.load())
 			{//等待策略
-				if (_stopWorking && empty())
+				if (_stopWorking.load() && empty())
 				{
 					return -1L;
 				}
@@ -133,38 +160,40 @@ namespace Kang {
 				return;
 			}
 
-			while (seq - 1 != _lastRead)
+			while (seq - 1 != _lastRead.load())
 			{//等待策略
 			}
-			_lastRead = seq;
+			//_lastRead = seq;
+			_lastRead.store(seq);
 		};
 
 		bool empty()
 		{
-			return _writableSeq.load() - _lastRead == 1;
+			return _writableSeq.load() - _lastRead.load() == 1;
 		}
 
 		//通知disruptor停止工作，调用该函数后，若buffer已经全部处理完，那么获取可读下标时只会获取到-1L
 		void stop()
 		{
-			_stopWorking = true;
+			//_stopWorking = true;
+			_stopWorking.store(1L);
 		}
 
 	private:
 		//最后一个已读内容位置
-		int64_t _lastRead;
+		Sequence _lastRead;
 
 		//最后一个已写内容位置
-		int64_t _lastWrote;
+		Sequence _lastWrote;
 
 		//disruptor是否停止工作
-		bool _stopWorking;
+		Sequence _stopWorking;
 
 		//最后一个派发给消费者使用的槽位序号
-		Sequence _lastDispatch;
+		AtomicSequence _lastDispatch;
 
 		//当前可写的槽位序号
-		Sequence _writableSeq;
+		AtomicSequence _writableSeq;
 
 		//环形buffer，为加快取余操作，N需要时2的n次幂
 		std::array<ValueType, N> _ringBuf;
